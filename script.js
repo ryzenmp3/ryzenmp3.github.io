823dbcfb4f536761cd5f3c6838d3f9f897778929
ryzenmp3 <64345339+ryzenmp3@users.noreply.github.com>
2021-10-08 20:20:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
             $('.cover').click(function () {
                 if (flag < 1)   
                 {
-                    window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
+                    window.open("https://shovedrailwaynurse.com/n75jjh1e?key=bc6ba36d46259171f28c22adb2cf0aab");
                 } else {  
                     $("body").removeClass("cover");
                     document.location.href="/";
