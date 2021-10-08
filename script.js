179cc3d236895d223b4c7ef9dbaf7e088526b809
ryzenmp3 <64345339+ryzenmp3@users.noreply.github.com>
2021-10-08 17:19:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
- var flag = 0;
-function myFunction(url) {
-       if(flag >= 1)    // jika di klik lebih dari 2 kali
-          {
-              if(url !== undefined){ // jika terdapat ada parameter d button akan redrect ke url tujuan
-                   window.location.href = "http://"+url;
-              }else{ // jika tidak ada parameter d button akan redrect ke homepage
-                  document.location.href="/";
-              }
-          }else{ // jika di klik kurang / sama dengan 2 kali akan redirect ke url iklan
-            window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
-          }
-          flag++;
-}
+ $(document).ready(function()
+  {
+    $('body').addClass('xepo_ads');
+  });
+
+  $(document).on('click','.xepo_ads',function(e)
+  {
+      $(this).removeClass('xepo_ads');
+      window.open('https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55', '_blank');
