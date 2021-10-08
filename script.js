bd803ed1c5e39583c5bd0454256699057936af49
ryzenmp3 <64345339+ryzenmp3@users.noreply.github.com>
2021-10-08 20:18:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     jQuery(function ($) {
             var flag = 0;
             $('.cover').click(function () {
-                if (flag < 2)   
+                if (flag < 1)   
                 {
                     window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
                 } else {  
