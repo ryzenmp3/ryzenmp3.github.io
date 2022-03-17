e9e3700f54391b857e510957fc697aa0f4299287
ryzenmp3 <64345339+ryzenmp3@users.noreply.github.com>
2022-03-17 14:09:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
             $('.cover').click(function () {
                 if (flag < 1)   
                 {
-                    window.open("https://rubberbeak.com/n75jjh1e?key=bc6ba36d46259171f28c22adb2cf0aab");
+                    window.open("https://tagstorkglory.com/n75jjh1e?key=bc6ba36d46259171f28c22adb2cf0aab");
                 } else {  
                     $("body").removeClass("cover");
                     document.location.href="/";
